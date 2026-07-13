5c7fed87e1674d3c09593b01b8495d6bd894286e
D Pragathi <pragathi123@gmail.com>
2026-07-13 11:46:21 +0530

    Initial Terraform project

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Azure Resource Group Name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
